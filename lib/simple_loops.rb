1e6dba9004386b93754897c2e35cceb0e7e9b733
Ekaterina Zarudnaya <zar.catherine@gmail.com>
2020-07-16 20:48:52 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,22 +24,22 @@ def output_array(message)
 end
 
 # 1st solution
-# def return_string_array(array)
-#   count = 0
-#   new_array = []
-#   while count < array.length do
-#     new_array.push(array[count].to_s) # OR new_array << array[count].to_s
-#     count += 1
-#   end
-#   new_array
-# end
-
-# 2nd solution
 def return_string_array(array)
   count = 0
+  new_array = []
   while count < array.length do
-    array[count] = array[count].to_s #modifying the passed in array, replacing each element with its string version
+    new_array.push(array[count].to_s) # OR new_array << array[count].to_s
     count += 1
   end
-  array
+  new_array
 end
+
+# 2nd solution
+# def return_string_array(array)
+#   count = 0
+#   while count < array.length do
+#     array[count] = array[count].to_s #modifying the passed in array, replacing each element with its string version
+#     count += 1
+#   end
+#   array
+# end
